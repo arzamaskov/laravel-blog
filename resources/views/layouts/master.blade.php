6c8a7660a32d8de57997ac2b9323ca7406bd7108
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-09 19:13:10 +0400

    Create layouts for bootstrap theme Album

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+@include ('layouts.head')
+
+<body>
+  <header>
+
+    @include ('layouts.nav')
+
+  </header>
+
+  <main role="main">
+
+    @yield ('content')
+
+  </main>
+
+  @include ('layouts.scripts')
+
+</body>
+
+</html>
