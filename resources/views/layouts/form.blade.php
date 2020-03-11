93a95177d8ddf7df7d81dad1a25c9011282e762a
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-11 23:51:44 +0400

    Add new view layouts

---
+++ resources/views/layouts/form.blade.php
@@ -0,0 +1,24 @@
+<form action="/search" method="get">
+    <div class="input-group">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">#</span>
+        </div>
+        <input type="text" class="form-control" placeholder="tag" aria-label="tag" aria-describedby="basic-addon1"
+            value="{{ $tag ?? '' }}" id="tag" name="tag">
+        <div class="input-group-append">
+            <select class="form-control text-center" name="number">
+                <option value="9" {{ $number == 9 ? 'selected' : ''}}>9</option>
+                <option value="12" {{ $number == 12 ? 'selected' : ''}}>12</option>
+                <option value="15" {{ $number == 15 ? 'selected' : ''}}>15</option>
+                <option value="18" {{ $number == 18 ? 'selected' : ''}}>18</option>
+                <option value="21" {{ $number == 21 ? 'selected' : ''}}>21</option>
+                <option value="24" {{ $number == 24 ? 'selected' : ''}}>24</option>
+                <option value="27" {{ $number == 27 ? 'selected' : ''}}>27</option>
+                <option value="30" {{ $number == 30 ? 'selected' : ''}}>30</option>
+            </select>
+        </div>
+    </div>
+    <small id="emailHelp" class="form-text text-muted mb-3">We'll never share what you type here with anyone
+        else.</small>
+    <button type="submit" class="btn btn-primary">Let's search on Instagram</button>
+</form>
---
+++ resources/views/show.blade.php
@@ -0,0 +1,39 @@
+@extends ('layouts.master')
+
+@section ('content')
+
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1>Instagram Grabber</h1>
+        <div class="msg">
+            <div class="lead text-muted">That's what we found for you. If you want more, try again.</div>
+        </div>
+
+        @include('layouts.form')
+
+    </div>
+</section>
+
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            @foreach ($urls as $url)
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="image-container">
+                        <img class="card-img-top" src="{{ $url }}">
+                    </div>
+                    <div class="card-body">
+                        <div class="btn-group d-flex justify-content-between align-items-center">
+                            <button type="button" class="btn btn-outline-secondary">View</button>
+                            <button type="button" class="btn btn-outline-secondary">Edit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+@endsection
