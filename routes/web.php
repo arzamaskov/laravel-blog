8b28b1cecf96d149e1f4c7545b5fff82d8096301
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-18 23:20:49 +0400

    Delete unnecessary blank lines ir route

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,8 @@
 use App\Http\Controllers\InstagramController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', 'InstagramController@index');
-
 Route::get('/search', 'InstagramController@search');
-
-
-// 1. Маршрут для добавления избранных
-
-// 2. Маршрут для удаления избранных
-
-// 3. Маршрут для показа избранных
 Route::get('/favorites', 'InstagramController@favorite');
 Route::post('/add', 'InstagramController@add');
 Route::post('/delete', 'InstagramController@delete');
